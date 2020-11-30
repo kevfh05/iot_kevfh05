453e6c220bd320cc7702c48c100f7353261b19ad
kevfh05 <kefh05@mail.com>
2020-11-30 10:29:07 -0600

    Código completo de arduino.

--- esp32_github/esp32_github.ino
+++ esp32_github/esp32_github.ino
@@ -8,40 +8,109 @@ WiFiMulti wifiMulti;
 ESP8266WiFiMulti wifiMulti;
 #endif
 
+#include <MQTT.h>
+
+WiFiClient net;
+MQTTClient client;
+
+long TiempoPasado = 0;
 #define ledIndicador 2
+#define led 5
+#define boton 4
+boolean estadoBoton = false;
+boolean estadoLed = false;
 
 void setup() {
   Serial.begin(115200);
   delay(10);
   Serial.println("Iniciando Sistema");
   pinMode(ledIndicador, OUTPUT);
-
+  pinMode(led, OUTPUT);
+  pinMode(boton, PULLDOWN);
   wifiMulti.addAP("CoWorking", "CoWorking$");
- // wifiMulti.addAP("SSID_FROM_AP_1", "YOUR_PASSWORD_FOR_AP_1");
-  //wifiMulti.addAP("SSID_FROM_AP_1", "YOUR_PASSWORD_FOR_AP_1");
+  wifiMulti.addAP("SSID_FROM_AP_1", "YOUR_PASSWORD_FOR_AP_1");
+  wifiMulti.addAP("SSID_FROM_AP_1", "YOUR_PASSWORD_FOR_AP_1");
+  client.begin("broker.shiftr.io", net);
+  client.onMessage(RecibirMensaje);
   Conectando();
 
 }
 
 void loop() {
-  Conectando();
+  client.loop();
+  delay(10);
+  if (!client.connected()) {
+    Conectando();
+  }
 
+  ActualizarLed();
+  ActualizarBoton();
+  MandarData();
+  
+}
+
+void MandarData() {
+  if (millis() > TiempoPasado + 500) {
+    String Mensaje = String(estadoBoton);
+    client.publish("/data/Boton", Mensaje);
+    TiempoPasado = millis();
+  }
+
+}
+
+void ActualizarLed() {
+  if (estadoLed) {
+    digitalWrite(led, 1);
+    Serial.println("Encendido");
+  }
+  else {
+    digitalWrite(led, 0);
+    Serial.println("Apagado");
+  }
+}
+
+void ActualizarBoton() {
+  if (digitalRead(boton) == 1) {
+    estadoBoton = !estadoBoton;
+    delay(500);
+  }
 }
 
 void Conectando() {
 
-  if (wifiMulti.run() != WL_CONNECTED) {
-    digitalWrite(ledIndicador, 1);
+  while (wifiMulti.run() != WL_CONNECTED) {
+    digitalWrite(ledIndicador, 0);
     delay(2000);
     digitalWrite(ledIndicador, 1);
     delay(2000);
-  } else {
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
+    Serial.println(".");
+  }
+  while (!client.connect("ControladorIOT", "dataKevfh05", "12345678")) {
+    digitalWrite(ledIndicador, 0);
+    delay(500);
+    digitalWrite(ledIndicador, 1);
+    delay(500);
+    Serial.println("*");
+  }
+  Serial.println("Conectado MQTT");
+  client.subscribe("/data/Led");
+
+}
+
+void RecibirMensaje(String &topic, String &payload) {
+  Serial.println("incoming: " + topic + " - " + payload);
+
+  if (payload == "1") {
+    estadoLed = true;
+  }
+  else {
+    estadoLed = false;
   }
 
+  // Note: Do not use the client in the callback to publish, subscribe or
+  // unsubscribe as it may cause deadlocks when other things arrive while
+  // sending and receiving acknowledgments. Instead, change a global variable,
+  // or push to a queue and handle it in the loop after calling `client.loop()`.
 }
 
 void EstadoWiFi() {

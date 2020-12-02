1f1abc670aecb46b35275d5686020110a40e8260
kevfh05 <kefh05@mail.com>
2020-12-02 15:57:33 -0600

    Update

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ let estadoFondo = false;
 // client = new Paho.MQTT.Client(BrokerMQTT, PuertoMQTT,'/mqtt-wss', ClienteIDMQTT);
 var client = mqtt.connect('wss://kevfh05~kevfh05@broker.shiftr.io', {
   clientId: ClienteIDMQTT,
-  protocolVersion: 3.1.1
+  protocolVersion: 3
 });
 
 // client.onConnectionLost = MQTTPerder;
